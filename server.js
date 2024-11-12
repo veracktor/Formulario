e3342477e7f067d02614f79104cf20f510957ce4
veracktor <veracktor@gmail.com>
2024-11-12 20:08:50 -0300

    Reordenadas algunas rutas para que levante scripts y style

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3000;
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
     res.send('¡Hola Mundo desde Express!');
-});
+  });
+
+  app.get('/inicio', (req, res) => {
+    res.sendFile(path.join(__dirname,'public/Formulario.html'));
+  });
+  
 
 app.listen(port, () => {
     console.log(`Servidor escuchando en http://localhost:${port}`);
